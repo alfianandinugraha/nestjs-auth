a9703bf594110c4b44ec331ebca3524425c7ab92
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-02-21 15:15:04 +0700

    feat(app): inject all controllers and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppService } from '@app/app.service';
 import { LoginController } from '@app/controllers/login.controller';
 import { ProfileController } from '@app/controllers/profile.controller';
+import { ProfileService } from '@app/services/profile.service';
+import { DatabaseService } from '@app/services/database.service';
+import { AuthService } from '@app/services/auth.service';
+import { RegisterController } from '@app/controllers/register/register.controller';
 
 @Module({
   imports: [],
-  controllers: [LoginController, ProfileController],
-  providers: [AppService],
+  controllers: [LoginController, ProfileController, RegisterController],
+  providers: [AppService, ProfileService, DatabaseService, AuthService],
 })
 export class AppModule {}
