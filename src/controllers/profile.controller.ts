2e1df3be0714d132084891c5dc953abadcfaff77
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-02-21 20:03:54 +0700

    feat(controller/profile): add UseGuards in class

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,17 +1,15 @@
-import { ProfileService } from '@app/services/profile.service';
-import { Controller, Get } from '@nestjs/common';
+import { TokenGuard } from '@app/guards/token.guard';
+import { FastifyGuardRequest } from 'fastify';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
 
 @Controller('/profile')
+@UseGuards(TokenGuard)
 export class ProfileController {
-  constructor(private readonly profileService: ProfileService) {}
-
   @Get('/')
-  async index() {
-    const profile = await this.profileService.get(1);
-
+  async index(@Req() req: FastifyGuardRequest) {
     return {
       message: "Successfully fetched user's profile",
-      data: profile,
+      data: req.user,
     };
   }
 }
