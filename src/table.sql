ed21422043d2f91045f8a56700f1ab76451d95c7
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-02-21 13:54:41 +0700

    feat(sql): create sql for create table

---
+++ src/table.sql
@@ -0,0 +1,20 @@
+-- SQLite
+DROP TABLE IF EXISTS "users";
+CREATE TABLE "users" (
+  "user_id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  "name" varchar(255) NOT NULL,
+  "email" varchar(255) UNIQUE NOT NULL,
+  "password" varchar(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS "tokens";
+CREATE TABLE tokens (
+  token TEXT PRIMARY KEY NOT NULL,
+  user_id INTEGER NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO users (name, email, password) VALUES ('John Doe', 'john@gmail.com', '123456');
+
