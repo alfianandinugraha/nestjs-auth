61be184d347bcfb5a39c053121a294be60b115fe
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-02-21 15:09:35 +0700

    feat(utils/hash): add static method compare

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -4,4 +4,8 @@ export class Hash {
   static make(value: string) {
     return createHash('sha256').update(value).digest('base64');
   }
+
+  static compare(value: string, hash: string) {
+    return Hash.make(value) === hash;
+  }
 }
