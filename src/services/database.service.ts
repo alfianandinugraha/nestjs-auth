507ea80f86abca7c42f1fb00d6ca461158561e34
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-02-21 13:56:11 +0700

    feat(services): create DatabaseService

---
+++ src/services/database.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { join } from 'node:path';
+import { Database } from 'sqlite3';
+
+@Injectable()
+export class DatabaseService {
+  private path = join(__dirname, '..', '..', 'db', 'db.sqlite');
+
+  /**
+   * @description
+   * Open a connection to the database
+   */
+  open() {
+    return new Database(this.path);
+  }
+
+  async get<T>(sql: string, conn: Database, param?: any) {
+    return new Promise<T>((resolve, reject) => {
+      conn.get(sql, param, function (err, row: T) {
+        if (err) return reject(err);
+        resolve(row);
+      });
+    });
+  }
+
+  async all<T>(sql: string, conn: Database, param?: any) {
+    return new Promise<T>((resolve, reject) => {
+      conn.all(sql, param, function (err: Error, row: T) {
+        if (err) return reject(err);
+        resolve(row);
+      });
+    });
+  }
+
+  async run<T>(sql: string, conn: Database, param?: any) {
+    return new Promise<T>((resolve, reject) => {
+      conn.run(sql, param, function (err: Error, row: T) {
+        if (err) return reject(err);
+        resolve(row);
+      });
+    });
+  }
+}
