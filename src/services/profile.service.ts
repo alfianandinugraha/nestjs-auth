2dece523c120a49da934f92c7a87ee2853058664
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-02-21 13:56:29 +0700

    feat(services): create ProfileService

---
+++ src/services/profile.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '@app/services/database.service';
+import User from '@app/models/user';
+import { Database } from 'sqlite3';
+
+@Injectable()
+export class ProfileService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async get(userId: number, conn?: Database) {
+    const db = conn ?? this.databaseService.open();
+
+    const user: User = await this.databaseService.get(
+      'SELECT * FROM users WHERE user_id = $userId OR email = $email;',
+      db,
+      {
+        $userId: userId,
+      },
+    );
+
+    !conn && db.close();
+    return user;
+  }
+
+  async findByEmail(email: string, conn?: Database) {
+    const db = conn ?? this.databaseService.open();
+
+    const user: User = await this.databaseService.get(
+      'SELECT * FROM users WHERE email = $email;',
+      db,
+      {
+        $email: email,
+      },
+    );
+
+    !conn && db.close();
+    return user;
+  }
+}
